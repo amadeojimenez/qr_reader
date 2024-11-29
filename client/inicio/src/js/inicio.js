66e395e03d15a927a2d7c032d3c31be01993a281
Diego <djunquerasaez@gmail.com>
2024-11-29 13:10:03 +0100

    avances

--- client/inicio/src/js/inicio.js
+++ client/inicio/src/js/inicio.js
@@ -1,12 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
     const videoElement = document.getElementById('video');
     const scanAreaElement = document.getElementById('scan-area');
     const statusElement = document.getElementById('status');
-    const menu = document.getElementById('menu');
-    const scanner = document.getElementById('scanner');
-    const backButton = document.getElementById('back-button');
-    let isScanning = true; 
-    let scanMode = ''; 
+    const modeIndicator = document.getElementById('mode-indicator');
+    const toggleModeButton = document.getElementById('toggle-mode-button');
+    let isScanning = true;
+    let scanMode = 'entrada'; // Default mode
 
     // Function to dynamically position the scan area
     function adjustScanArea() {
@@ -29,94 +28,139 @@ $(document).ready(function() {
 
     // Function to send QR data to the backend
     function sendQRCodeData(qrData) {
-
         const url = scanMode === 'entrada' ? '/qrReader/entrada' : '/qrReader/salida';
 
         return $.ajax({
             url: url,
             type: 'POST',
-            data: {qr_data: qrData} ,
-            success: function(response) {
-                console.log(response,'response')
-                return response;  //{ status: "approved" | "denied" }
+            data: { qr_data: qrData },
+            success: function (response) {
+                console.log(response, 'response');
+                return response; // { status: "approved" | "denied" }
             },
-            error: function(err) {
+            error: function (err) {
                 console.error('Error:', err);
-                return { status: 'error' }; 
+                return { status: 'error' };
             }
         });
     }
 
-      //Update UI en función de la respuesta
-      function handleResponse(status) {
-        if (status === 'approved') {
-            scanAreaElement.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
-            statusElement.textContent = 'QR válido!';
-        } else if (status === 'denied') {
-            scanAreaElement.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
-            statusElement.textContent = 'QR inválido o ya usado!';
-        } else {
-            statusElement.textContent = 'Error procesando QR!';
-        }
+    // Update UI based on the response
+    function handleResponse(status) {
 
-        setTimeout(() => {
-            scanAreaElement.style.backgroundColor = 'rgba(138, 138, 138, 0.2)';
+        const audioValidated = new Audio('../sounds/valid.mp3');
+        const audioUnvalidated = new Audio('../sounds/unvalid.mp3');
+
+        if(scanMode == 'entrada'){
+
+            if (status === 'approved') {
+                scanAreaElement.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+                statusElement.textContent = 'QR válido!';
+                audioValidated.play();
+
+            const botonPasar = document.getElementById('pasar-button');
+            botonPasar.style.display = 'flex';
+
+         
+            botonPasar.onclick = () => {
+                resetScanner();
+                botonPasar.style.display = 'none';
+            };
+
+            } else if (status === 'denied') {
+                scanAreaElement.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+                statusElement.textContent = 'QR inválido o ya usado!';
+                audioUnvalidated.play()
+                
+            } else {
+                statusElement.textContent = 'Error procesando QR!';
+            }
+    
+        const timeoutId = setTimeout(() => {
+            resetScanner();
+            const botonPasar = document.getElementById('pasar-button');
+            botonPasar.style.display = 'none';
+        }, 2000);
+
+
+        function resetScanner() {
+            scanAreaElement.style.backgroundColor = 'transparent';
             statusElement.textContent = 'Apunta la cámara hacia el QR.';
             isScanning = true;
-        }, 2000);
+            clearTimeout(timeoutId); 
+        }
+
+        }
+        else {
+
+            if (status === 'approved') {
+                scanAreaElement.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+                statusElement.textContent = 'QR válido!';
+                audioValidated.play();
+            } else if (status === 'denied') {
+                scanAreaElement.style.backgroundColor = 'rgba(255, 0, 0, 0.5)';
+                statusElement.textContent = 'QR inválido o ya usado!';
+                audioUnvalidated.play()
+            } else {
+                statusElement.textContent = 'Error procesando QR!';
+            }
+    
+            setTimeout(() => {
+                scanAreaElement.style.backgroundColor = 'transparent';
+                statusElement.textContent = 'Apunta la cámara hacia el QR.';
+                isScanning = true;
+            }, 2000);
+
+
+        }
+       
     }
 
     // Initialize the QR Scanner
-        const qrScanner = new QrScanner(
-            videoElement,
-            result => {
-                if (isScanning) {
-                    console.log('Decoded QR Code:', result);
-                    isScanning = false;  // deshabilitamos el escaneo hasta recibir respuesta del backend
-                    statusElement.textContent = 'Procesando QR...';
+    const qrScanner = new QrScanner(
+        videoElement,
+        (result) => {
+            if (isScanning) {
+                console.log('Decoded QR Code:', result);
+                isScanning = false; // Disable scanning until response is received
+                statusElement.textContent = 'Procesando QR...';
 
-                    // Enviamos la data del qr al backend
-                    sendQRCodeData(result)
-                        .then(response => {
-                            handleResponse(response.status);
-                        });
-                }
-            },
-        );
+                // Send QR data to the backend
+                sendQRCodeData(result)
+                    .then((response) => {
+                        handleResponse(response.status);
+                    });
+            }
+        }
+    );
 
-        function startQrScanner() {
-            qrScanner.start()
+    function startQrScanner() {
+        qrScanner
+            .start()
             .then(() => {
                 statusElement.textContent = 'Apunta la cámara hacia el QR.';
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Camera initialization failed:', error);
-                statusElement.textContent = 'No se ha podido acceder a la cámara. Verifica que se han habilitado los permisos.';
+                statusElement.textContent =
+                    'No se ha podido acceder a la cámara. Verifica que se han habilitado los permisos.';
             });
-        }
+    }
+
+    // Toggle scan mode
+    toggleModeButton.addEventListener('click', function () {
+        scanMode = scanMode === 'entrada' ? 'salida' : 'entrada';
+
+        // Update the UI based on the new mode
+        modeIndicator.textContent = scanMode.toUpperCase();
+        modeIndicator.className = scanMode; // Change background color
+        scanAreaElement.style.borderColor = scanMode === 'entrada' ? 'rgba(0, 128, 0, 0.649)' : 'rgba(0, 0, 255,  0.649)'; 
         
-        //  Event Listeners
-    $('#entrada-button').on('click', function () {
-        scanMode = 'entrada';
-        menu.style.display = 'none';
-        scanner.style.display = 'block';
-        startQrScanner();
-        adjustScanArea();
+        toggleModeButton.innerHTML = scanMode === 'entrada'
+            ? '<i class="fa-solid fa-right-from-bracket"></i>'
+            : '<i class="fa-solid fa-person-walking"></i>';
     });
 
-    $('#salida-button').on('click', function () {
-        scanMode = 'salida';
-        menu.style.display = 'none';
-        scanner.style.display = 'block';
-        startQrScanner();
-        adjustScanArea();
-    });
-
-    backButton.addEventListener('click', function () {
-        qrScanner.stop();
-        scanner.style.display = 'none';
-        menu.style.display = 'flex';
-    });
-       
+    // Start scanning
+    startQrScanner();
 });
-
